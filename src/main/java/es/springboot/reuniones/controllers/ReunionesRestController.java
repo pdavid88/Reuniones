6adf9be3c939bd5a7aa0340d411d15b4c30e517e
pdavid88 <david.alg88@gmail.com>
2024-08-18 20:27:27 +0200

    Reuniones 1.6 - Creacion ReunionesRestController

---
+++ src/main/java/es/springboot/reuniones/controllers/ReunionesRestController.java
@@ -0,0 +1,26 @@
+package es.springboot.reuniones.controllers;
+
+import es.springboot.reuniones.models.Reunion;
+import es.springboot.reuniones.services.ReunionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reuniones")
+public class ReunionesRestController {
+
+    private ReunionService reunionService;
+
+    public ReunionesRestController(ReunionService reunionService) {
+        this.reunionService = reunionService;
+    }
+
+    @GetMapping
+    public List<Reunion> getAllReuniones() {
+        return reunionService.getAllReuniones();
+    }
+
+}
